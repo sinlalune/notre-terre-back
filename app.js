ff240792b2b64019fc86c3bcc0908637d486c792
KlrD13 <claire.dambrin@hotmail.fr>
2022-08-03 14:53:56 +0200

    midif research route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var createRouter = require("./routes/create");
 var cardRouter = require("./routes/card");
+var researchRouter = require('./routes/research')
 
 var app = express();
 
