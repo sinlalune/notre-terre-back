21ad9cb191c05971170bd5d251d888f70e514501
Camille Courgibet <camille.courgibet@gmail.com>
2022-08-05 15:13:15 +0200

    aucune modif

--- routes/card.js
+++ routes/card.js
@@ -5,26 +5,26 @@ const productModel = require("../models/products");
 const producerModel = require("../models/producers");
 
 router.get("/productlist", async function (req, res, next) {
-  const product = await productModel.find();
-  res.json({ product });
+	const product = await productModel.find();
+	res.json({ product });
 });
 
 router.get("/producer", async function (req, res, next) {
-  console.log("req", req.query.producer_id);
-  const producer_id = req.query.producer_id;
-  console.log("id", producer_id);
-  const producer = await producerModel.findById(producer_id.toString());
-  console.log("producer", producer);
-  res.json({ producer });
+	console.log("req", req.query.producer_id);
+	const producer_id = req.query.producer_id;
+	console.log("id", producer_id);
+	const producer = await producerModel.findById(producer_id.toString());
+	console.log("producer", producer);
+	res.json({ producer });
 });
 
 router.get("/product", async function (req, res, next) {
-  const product_id = req.query.product_id;
-  console.log("The id of this product is: ", product_id);
-  const product = await productModel
-    .findById(product_id)
-    .populate("domain_adress");
-  res.json({ product });
+	const product_id = req.query.product_id;
+	console.log("The id of this product is: ", product_id);
+	const product = await productModel
+		.findById(product_id)
+		.populate("domain_adress");
+	res.json({ product });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -19,90 +19,102 @@ var userModel = require("../models/users");
 var cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "CHANGE CLOUD NAME",
-  api_key: "CHANGE API KEY",
-  api_secret: "CHANGE API SECRET",
+	cloud_name: "CHANGE CLOUD NAME",
+	api_key: "CHANGE API KEY",
+	api_secret: "CHANGE API SECRET",
 });
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+	res.send("respond with a resource");
 });
 
 /* POST user to database. */
 router.post("/sign-up", async function (req, res, next) {
-  console.log(req.body);
+	console.log(req.body);
 
-  const cost = 10;
-  const hash = bcrypt.hashSync(req.body.passwordFromFront, cost);
+	const cost = 10;
+	const hash = bcrypt.hashSync(req.body.passwordFromFront, cost);
 
-  var error = [];
-  var result = false;
-  var saveUser = null;
-  var token = null;
+	var error = [];
+	var result = false;
+	var saveUser = null;
 
-  const data = await userModel.findOne({
-    email: req.body.emailFromFront,
-  });
+	const data = await userModel.findOne({
+		email: req.body.emailFromFront,
+	});
 
-  if (data) {
-    error.push(
-      "❌ Utilisateur déjà enregistré, veuillez saisir une autre adresse email ❌"
-    );
-  }
+	if (data) {
+		error.push(
+			"❌ Utilisateur déjà enregistré, veuillez saisir une autre adresse email ❌",
+		);
+	}
 
-  if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
-    error.push("❌ Ooops, j'ai besoin de plus d'informations ❌");
-  }
+	if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
+		error.push("❌ Ooops, j'ai besoin de plus d'informations ❌");
+	}
 
-  if (error.length == 0) {
-    var newUser = new userModel({
-      email: req.body.emailFromFront,
-      password: hash,
-      token: uid2(32),
-    });
+	if (error.length == 0) {
+		var newUser = new userModel({
+			email: req.body.emailFromFront,
+			password: hash,
+			token: uid2(32),
+			avatar:
+				"https://res.cloudinary.com/matthieudev/image/upload/v1659625192/generic-avatar_mpp1wf.png",
+		});
 
-    saveUser = await newUser.save();
+		saveUser = await newUser.save();
 
-    if (saveUser) {
-      result = true;
-      token = saveUser.token;
-    }
-  }
+		if (saveUser) {
+			result = true;
+			token = saveUser.token;
+			saveUser = {
+				avatar: saveUser.avatar,
+				email: saveUser.email,
+				token: saveUser.token,
+			};
+		}
+	}
 
-  res.json({ result, searchUser: saveUser, error, token });
+	res.json({ result, searchUser: saveUser, error });
 });
 
 // POST existing user
 router.post("/sign-in", async function (req, res, next) {
-  var error = [];
-  var result = false;
-  var searchUser = null;
-  var token = null;
+	console.log(req.body);
+	var error = [];
+	var result = false;
+	var searchUser = null;
 
-  if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
-    error.push("❌ Ooops, j'ai besoin de plus d'informations ❌");
-  }
+	if (req.body.emailFromFront == "" || req.body.passwordFromFront == "") {
+		error.push("❌ Ooops, j'ai besoin de plus d'informations ❌");
+	}
 
-  if (error.length == 0) {
-    var searchUser = await userModel.findOne({
-      email: req.body.emailFromFront,
-    });
-  }
+	if (error.length == 0) {
+		var searchUser = await userModel.findOne({
+			email: req.body.emailFromFront,
+		});
+		console.log(searchUser);
+	}
 
-  if (searchUser) {
-    if (bcrypt.compareSync(req.body.passwordFromFront, searchUser.password)) {
-      result = true;
-    } else {
-      result = false;
-      searchUser = null;
-      error.push("❌ Email ou mot de passe incorrect ❌");
-    }
-  } else {
-    error.push("❌ Email ou mot de passe incorrect ❌");
-  }
+	if (searchUser) {
+		if (bcrypt.compareSync(req.body.passwordFromFront, searchUser.password)) {
+			searchUser = {
+				avatar: searchUser.avatar,
+				email: searchUser.email,
+				token: searchUser.token,
+			};
+			result = true;
+		} else {
+			result = false;
+			searchUser = null;
+			error.push("❌ Email ou mot de passe incorrect ❌");
+		}
+	} else {
+		error.push("❌ Email ou mot de passe incorrect ❌");
+	}
 
-  res.json({ result, searchUser, token, error });
+	res.json({ result, searchUser, error });
 });
 
 module.exports = router;
